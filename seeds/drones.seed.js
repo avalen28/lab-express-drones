ce82171cf2cda4ec6a1ea6edc028034c2e874477
Alberto Valenzuela <avalen28@gmail.com>
2023-01-11 18:40:23 +0100

    iteration 1 done. Model and seed created. Conection with Mongoose done and Collection created

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,24 @@
-// Iteration #1
+// Iteration #1  ----- collection and Mongoose call
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+const Drone = require("../models/Drone.model");
+
+const MONGO_URL = "mongodb://localhost:27017/drones";
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => console.log("conected with Mongo"))
+  .then(() => Drone.deleteMany())
+  .then(() => Drone.create(drones))
+  .then((dronesData) => console.log(`${dronesData.length} Drones created`))
+  .catch((err) => console.log(err))
+  .finally(() => mongoose.disconnect());
+
+module.exports = drones;
